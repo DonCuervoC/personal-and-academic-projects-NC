bb6b92daea0a65f41f9b5f8b3ad14152f3a0577b
DonCuervoC <nelson.cuervo89@gmail.com>
2023-06-19 14:42:32 -0400

    API - USER : Dto (User,lohin,Response,Register) OK

---
+++ ASP.NET Core Web API/API RESTful/ApiFilms/Models/Dtos/UserLoginDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiFilms.Models.Dtos
+{
+    public class UserLoginDto
+    {
+        [Required(ErrorMessage = "User-Name is mandatory")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Password is mandatory")]
+        public string Password { get; set; }
+
+    }
+}
