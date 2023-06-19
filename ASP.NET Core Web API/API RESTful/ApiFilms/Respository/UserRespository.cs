e1e77ae5a8291f2b7a2d971446092891e8e60c91
DonCuervoC <nelson.cuervo89@gmail.com>
2023-06-19 15:05:59 -0400

    API - USER : Interface Implementation

---
+++ ASP.NET Core Web API/API RESTful/ApiFilms/Respository/UserRespository.cs
@@ -0,0 +1,50 @@
+﻿using ApiFilms.Data;
+using ApiFilms.Models;
+using ApiFilms.Models.Dtos;
+using ApiFilms.Respository.IRepository;
+
+namespace ApiFilms.Respository
+{
+    public class UserRespository : IUserRepository
+    {
+
+        private readonly ApplicationDbContext _db;
+
+        public UserRespository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public User GetUser(int userId)
+        {
+            return _db.User.FirstOrDefault(u => u.Id == userId);
+        }
+
+        public ICollection<User> GetUsers()
+        {
+            return _db.User.OrderBy(u => u.UserName).ToList();
+        }
+
+        public bool IsUniqueUser(string user)
+        {
+            var userDb = _db.User.FirstOrDefault(u => u.UserName == user);
+
+            if (userDb == null)
+            {
+                return true;
+            }
+            return false;
+
+        }
+
+        public Task<UserLoginResponseDto> Login(UserLoginDto userLoginDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> Register(UserRegisterDto userRegisterDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
