fa96c7e3a55a54303cd9d789da05ed5e8d3abebd
DonCuervoC <nelson.cuervo89@gmail.com>
2023-06-29 15:29:59 -0400

    Consume API film : Models OK

---
+++ ASP.NET Core Web API/API RESTful/FilmsWeb_GgraphInterface/Core 5.1/FilmsWebCore5/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System;
+
+namespace FilmsWebCore5.Models
+{
+    public class Category
+    {
+
+        public int Id { get; set; }
+        [Required(ErrorMessage ="Name is mandatory")]
+        public string Name { get; set; }
+
+        public DateTime DateCreation { get; set; }
+
+    }
+}
---
+++ ASP.NET Core Web API/API RESTful/FilmsWeb_GgraphInterface/Core 7/FilmsWebCore7/Models/Film.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace FilmsWebCore5.Models
+{
+    public class Film
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Name is mandatory")]
+        public string Name { get; set; }
+        public byte[] FilmPath { get; set; }  // This data indicates that the image will be saved directly to the database.
+        [Required(ErrorMessage = "Description is mandatory")]
+        public string Description { get; set; }
+        public int Duration { get; set; }
+        public enum ClassificationType { seven, thirteen, sixteen, eighteen }
+
+        public ClassificationType Classification { get; set; }
+        public DateTime CreationDate { get; set; }
+
+        public int categoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
